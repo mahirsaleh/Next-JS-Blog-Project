0fdd0424a8fc184019669c6c723a6cbef4558e55
Mahir Saleh <mahir-saleh@mahir-saleh.com>
2026-06-25 20:20:27 +0600

    solved the previous commit problems in proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -2,24 +2,24 @@ import { getSessionCookie } from "better-auth/cookies";
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/Blog", "/Create", "/LogIn", "/SignIn", "/Blog/:id*"],
+  matcher: ["/Blog", "/Create", "/Blog/:id*", "/LogIn", "/SignIn"],
 };
 
 export function proxy(request: NextRequest) {
   const sesstionCookie = getSessionCookie(request);
   const { pathname } = request.nextUrl;
 
-  if (!sesstionCookie) {
-    const isTryToSeeData = ["/Blog", "/Create", "/LogIn", "/Blog/:id*"].some(
-      (singlePath) => pathname.startsWith(singlePath),
-    );
+  const DataRouteArray = ["/Blog", "/Create", "/Blog/:id*"];
+  const isTryToSeeData = DataRouteArray.some((singlePath) =>
+    pathname.startsWith(singlePath),
+  );
 
-    if (isTryToSeeData) {
-      return NextResponse.redirect(new URL("/LogIn", request.url));
-    }
+  if (!sesstionCookie && isTryToSeeData) {
+    return NextResponse.redirect(new URL("/LogIn", request.url));
   }
 
-  const isLogInUserWantToGoAuthPages = ["/SignIn", "/LogIn"].includes(pathname);
+  const authRouteArray = ["/SignIn", "/LogIn"];
+  const isLogInUserWantToGoAuthPages = authRouteArray.includes(pathname);
 
   if (sesstionCookie && isLogInUserWantToGoAuthPages) {
     return NextResponse.redirect(new URL("/", request.url));
