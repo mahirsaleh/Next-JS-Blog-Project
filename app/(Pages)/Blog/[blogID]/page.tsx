d90373612d23e2a1f9240dfa47f15973b4205a9e
Mahir Saleh <mahir-saleh@mahir-saleh.com>
2026-06-25 13:57:42 +0600

    I am done

--- app/(Pages)/Blog/[blogID]/page.tsx
+++ app/(Pages)/Blog/[blogID]/page.tsx
@@ -1,6 +1,9 @@
-import { getBlogCommentFunction, singleBlogFunction } from "@/app/actions";
+// import { getBlogCommentFunction, singleBlogFunction } from "@/app/actions";
+
 import { Id } from "@/convex/_generated/dataModel";
+
 import { notFound } from "next/navigation";
+
 import Image from "next/image";
 
 import BlogComments from "./_singleBlogComments/BlogComments";
@@ -8,47 +11,58 @@ import BlogComments from "./_singleBlogComments/BlogComments";
 import MadinaImage from "@/src/assets/Medina 1.jpg";
 
 import "@/src/css/singleBlogPage.css";
+
 import { Metadata } from "next";
 
 // import { cacheLife, cacheTag } from "next/cache";
+
 import BlogDeleteButton from "./_blogDeleteButtons/BlogDeleteButton";
+
 import DeletePopUp from "./_blogDeleteButtons/DeletePopUp";
+
 import DeletePostWrapper from "@/src/contextProviders/DeletePostWrapper";
+
 import { fetchQuery, preloadQuery } from "convex/nextjs";
+
 import { api } from "@/convex/_generated/api";
 
 type TProps = {
   params: Promise<{ blogID: Id<"posts"> }>;
 };
 
-export async function generateMetadata({ params }: TProps): Promise<Metadata> {
-  try {
-    const { blogID } = await params;
-    const singleBlogData = await fetchQuery(api.posts.singleBlogWithID, {
-      postID: blogID,
-    });
+// export async function generateMetadata({ params }: TProps): Promise<Metadata> {
+//   try {
+//     const { blogID } = await params;
 
-    if (!singleBlogData) {
-      return {
-        title: "No Blog Found",
-        description: "Currently there is no blog has been posted",
-      };
-    }
+//     const singleBlogData = await fetchQuery(api.posts.singleBlogWithID, {
+//       postID: blogID,
+//     });
 
-    return {
-      title: singleBlogData.title,
-      description: `This page is a blog page about ${singleBlogData.title}`,
-    };
-  } catch (error) {
-    throw error;
-  }
-}
+//     if (!singleBlogData) {
+//       return {
+//         title: "No Blog Found",
+
+//         description: "Currently there is no blog has been posted",
+//       };
+//     }
+
+//     return {
+//       title: singleBlogData.title,
+
+//       description: `This page is a blog page about ${singleBlogData.title}`,
+//     };
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
 export default async function SingleBlog({ params }: TProps) {
   // cacheLife("max");
 
   let singleBlogData;
+
   // let blogId;
+
   let preloadedComments;
 
   try {
@@ -57,12 +71,16 @@ export default async function SingleBlog({ params }: TProps) {
     // blogId = blogID;
 
     // [singleBlogData, preloadedComments] = await Promise.all([
-    //   singleBlogFunction({ postID: blogId }),
-    //   getBlogCommentFunction({ blogID: blogId }),
+
+    // singleBlogFunction({ postID: blogId }),
+
+    // getBlogCommentFunction({ blogID: blogId }),
+
     // ]);
 
     [singleBlogData, preloadedComments] = await Promise.all([
       fetchQuery(api.posts.singleBlogWithID, { postID: blogID }),
+
       preloadQuery(api.postComments.getPostComments, { postID: blogID }),
     ]);
 
@@ -77,13 +95,16 @@ export default async function SingleBlog({ params }: TProps) {
     <div className="single-blog-container">
       <div className="single-blog-container__post-header">
         <h1 className="post-header__post-title">{singleBlogData.title}</h1>
+
         <p className="post-header__post-creation-time">
           {new Date(singleBlogData._creationTime!).toLocaleString()}
         </p>
+
         <p className="post-header__post-creation-time post-header__authorName">
           Author Name - {singleBlogData.authorName}
         </p>
       </div>
+
       <div className="single-blog-container__single-blog-container-image-div">
         <Image
           className="single-blog-contaier-image-div__image"
@@ -94,6 +115,7 @@ export default async function SingleBlog({ params }: TProps) {
           loading="eager"
         />
       </div>
+
       <p className="single-blog-container__post-body">{singleBlogData.body}</p>
 
       <DeletePostWrapper>
